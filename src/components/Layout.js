6694ec051655779f0697f2d16dcd9e4f80a7a904
Benjamin Thomas <bhopthomas@gmail.com>
2020-10-03 15:28:03 +0100

    gtm integration

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,7 +21,20 @@ export default class Body extends React.Component {
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
                     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
                     <link rel="manifest" href="/images/favicon/site.webmanifest"></link>
+                    
+                    
+                    <!-- Google Tag Manager -->
+                    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-P8TRRWD');</script>
+                    <!-- End Google Tag Manager -->
                 </Helmet>
+                <!-- Google Tag Manager (noscript) -->
+                    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P8TRRWD"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+                <!-- End Google Tag Manager (noscript) -->
                 <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette', null)}>
                   <Header {...this.props} />
                   <main id="content" className="site-content">
