d912bac5029fccf60abf48a0f4ae06b6700aaaad
Benjamin Thomas <beetee@Benjamins-MacBook-Pro.local>
2020-10-04 14:50:28 +0100

    footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import _ from 'lodash';
 
 import {Link, withPrefix, htmlToReact} from '../utils';
 import ActionLink from './ActionLink';
-import SubscribeForm from './SubscribeForm';
 
 export default class Footer extends React.Component {
     render() {
@@ -58,13 +57,17 @@ export default class Footer extends React.Component {
                       </div>
                     </nav>
                     )}
-                    {_.get(this.props, 'pageContext.site.siteMetadata.footer.has_subscribe', null) && (
-                    <div className="widget footer-subscribe">
-                      <h2 className="widget-title">{_.get(this.props, 'pageContext.site.siteMetadata.footer.subscribe_title', null)}</h2>
-                      {_.get(this.props, 'pageContext.site.siteMetadata.footer.subscribe_content', null) && (
-                      <p>{htmlToReact(_.get(this.props, 'pageContext.site.siteMetadata.footer.subscribe_content', null))}</p>
+                    {_.get(this.props, 'pageContext.site.siteMetadata.footer.has_contact', null) && (
+                    <div className="widget footer-contact">
+                      <h2 className="widget-title">{_.get(this.props, 'pageContext.site.siteMetadata.footer.contact_title', null)}</h2>
+                      {_.get(this.props, 'pageContext.site.siteMetadata.footer.contact_phone', null) && (
+                      <p><strong>Phone: </strong><Link to="Tel:">{htmlToReact(_.get(this.props, 'pageContext.site.siteMetadata.footer.contact_phone', null))}</Link></p>
                       )}
-                      <SubscribeForm {...this.props} />
+                      {_.get(this.props, 'pageContext.site.siteMetadata.footer.contact_address', null) && (
+                      <p><strong>Address: </strong>{htmlToReact(_.get(this.props, 'pageContext.site.siteMetadata.footer.contact_address', null))}</p>
+                      )}
+                      <p><Link to="https://www.google.com/maps/dir//Vantage+Chiropractic,+Invictus+Gym,+3+Wenman+Rd,+Thame+OX9+3SD/@51.7390425,-0.9671288,19z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4876ed6954ea947b:0x1cbb4aa56e5c2e33!2m2!1d-0.9679445!2d51.7395462"><strong>Get Directions</strong></Link></p>
+                      
                     </div>
                     )}
                   </div>
@@ -72,6 +75,9 @@ export default class Footer extends React.Component {
               </div>
               <div className="site-info outer">
                 <div className="inner">
+                <p className="accreditation-logo">
+                        <img src={withPrefix(_.get(this.props, 'pageContext.site.siteMetadata.footer.accreditation_img', null))} alt="General Chiropractic Council Registered" />
+                      </p>
                   {htmlToReact(_.get(this.props, 'pageContext.site.siteMetadata.footer.content', null))}
                   &nbsp;
                   {_.map(_.get(this.props, 'pageContext.site.siteMetadata.footer.links', null), (action, action_idx) => (
